affc57006ffa5700a9ab226934739c8e4fe2e0bd
DanhezCode <danhezcode@gmail.com>
2026-08-21 17:13:31 -0400

    chore: start basic cspell configuration

---
+++ cspell.config.ts
@@ -0,0 +1,23 @@
+import type { CSpellSettings } from "cspell";
+
+export default {
+  dictionaries: [
+    "typescript",
+    "git",
+    "bash",
+    "sql",
+    "filetypes",
+    "node",
+    "npm",
+    "html",
+    "css",
+    "json",
+    "markdown",
+    "softwareTerms",
+  ],
+  files: ["**/*.{js,ts,jsx,tsx,txt,md}"],
+  ignorePaths: ["node_modules", "dist", "coverage", "build"],
+  import: ["@cspell/dict-es-es/cspell-ext.json"],
+  language: "en,es,es-ES",
+  words: ["oxlint", "oxfmt", "tsgolint"],
+} satisfies CSpellSettings;
